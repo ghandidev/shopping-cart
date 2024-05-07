5df76abe81fbf934eb264355844a1a949c06162e
ghandidev <ghandiaboultaif@gmail.com>
2024-05-07 17:49:03 -0400

    get elements from the DOM

---
+++ script.js
@@ -0,0 +1,3 @@
+const cartContainer = document.getElementById('cart-container');
+const productsContainer = document.getElementById('products-container');
+const dessertCards = document.getElementById('dessert-card-container');
