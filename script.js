8181967abb8310121e0ba039a4856a3d62d6b288
ghandidev <ghandiaboultaif@gmail.com>
2024-05-07 17:51:27 -0400

    get btn from the DOM

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const cartContainer = document.getElementById('cart-container');
 const productsContainer = document.getElementById('products-container');
 const dessertCards = document.getElementById('dessert-card-container');
+
+const cartBtn = document.getElementById('cart-btn');
+const clearCartBtn = document.getElementById('clear-cart-btn');
