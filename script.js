d9b93a0f87aa2a3c372fbac76d09e9e407623e4f
ghandidev <ghandiaboultaif@gmail.com>
2024-05-07 17:54:55 -0400

    get more elements from the DOM

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const dessertCards = document.getElementById('dessert-card-container');
 
 const cartBtn = document.getElementById('cart-btn');
 const clearCartBtn = document.getElementById('clear-cart-btn');
+
+const totalNumberOfItems = document.getElementById('total-items');
+const cartSubTotal = document.getElementById('subtotal');
+const cartTaxes = document.getElementById('taxes');
+const cartTotal = document.getElementById('total');
